bf73209130f0ccfcde74f8e7b98cdd54973a1d7f
Anthony <abr23@live.nl>
2016-01-20 14:03:49 +0100

    runt m niet helemaal, ben ermee bezig, maar alvast erin gezet

--- GameBoard.py
+++ GameBoard.py
@@ -1,4 +1,4 @@
-import pygame , sys
+import pygame, sys
 from pygame .locals import *
 
 pygame.init()
@@ -8,6 +8,21 @@ backPic = pygame.image.load('Oak.jpg')
 DISPLAYSURF.blit(pygame.transform.scale(backPic, (1150, 700)), (0, 0))
 pic = pygame.image.load("Bord.png")
 pygame.display.flip()
+
+Greensqr = pygame.image.load("BoardGame Greensquare")
+Bluesquare = pygame.image.load("BoardGame Bluesquare")
+yellowrect = pygame.image.load("BoardGame Yellowsquare")
+redsquare = pygame.image.load("BoardGame Redsquare")
+greyrect = pygame.image.load("BoardGame greyrect")
+whiterect = pygame.image.load("BoardGame whiterect")
+fightrect = pygame.image.load("BoardGame fightrect")
+bluerect = pygame.image.load("BoardGame bluerect")
+redrect = pygame.image.load("BoardGame redrect")
+greenrect = pygame.image.load("BoardGame greenrect")
+yellowrect = pygame.image.load("BoardGame yellowrect")
+
+
+
 
 while True:
     DISPLAYSURF.blit(pygame.transform.scale(pic, (950, 575)), (0, 0))
