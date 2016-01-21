a4bc12e762bdae306d0a52fd5f02d3295212fdd4
thelizardbrain <at_polat@hotmail.com>
2016-01-21 13:30:11 +0100

    ..

--- Menu + Instructions + Exit Buttons.py
+++ Menu + Instructions + Exit Buttons.py
@@ -16,6 +16,107 @@ white = (255,255,255)
 red = (255,20,0)
 dark_red = (150,0,0)
 
+def Gameboard():
+
+    YELLOW = (255, 255, 102)
+    ANDERSGEEL = (204, 204, 0)
+    RED = (201, 57, 57)
+    ANDERSROOD = (153, 0, 0)
+    GREEN = (102,   204, 0  )
+    ANDERSGROEN = (0, 102, 0)
+    BLUE  = (0,   102,   205)
+    ANDERSBLAUW = (0, 51, 102)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GREY = (224, 224, 224)
+
+
+
+
+    #constants representing the different resources
+    ROODVAK  = 0
+    GROENVAK = 1
+    BLAUWVAK = 2
+    GEELVAK  = 3
+    WITVAK = 4
+    GRIJSVAK = 5
+    ZWARTVAK = 6
+    ANDERSGROENVAK = 7
+    ANDERSGEELVAK = 8
+    ANDERSROODVAK = 9
+    ANDERSBLAUWVAK = 10
+
+
+    #a dictionary linking resources to colours
+    colours =   {
+                    ROODVAK  : RED,
+                    GROENVAK : GREEN,
+                    BLAUWVAK : BLUE,
+                    GEELVAK  : YELLOW,
+                    WITVAK : WHITE,
+                    GRIJSVAK : GREY,
+                    ZWARTVAK : BLACK,
+                    ANDERSGROENVAK : ANDERSGROEN,
+                    ANDERSGEELVAK : ANDERSGEEL,
+                    ANDERSROODVAK : ANDERSROOD,
+                    ANDERSBLAUWVAK : ANDERSBLAUW,
+                }
+
+    #a list representing our tilemap
+    tilemap = [
+                [ANDERSBLAUWVAK, BLAUWVAK, GRIJSVAK, WITVAK, GRIJSVAK, WITVAK, GRIJSVAK, WITVAK, GRIJSVAK, ROODVAK, ANDERSROODVAK],
+                [BLAUWVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ROODVAK],
+                [GRIJSVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, GRIJSVAK],
+                [WITVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, WITVAK],
+                [GRIJSVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, GRIJSVAK],
+                [WITVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, WITVAK],
+                [GRIJSVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, GRIJSVAK],
+                [WITVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, WITVAK],
+                [GRIJSVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, GRIJSVAK],
+                [GEELVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, ZWARTVAK, GROENVAK],
+                [ANDERSGEELVAK, GEELVAK, GRIJSVAK, WITVAK, GRIJSVAK, WITVAK, GRIJSVAK, WITVAK, GRIJSVAK, GROENVAK, ANDERSGROENVAK]
+              ]
+
+    #useful game dimensions
+    TILESIZE  = 50
+    MAPWIDTH  = 11
+    MAPHEIGHT = 11
+
+    #set up the display
+    pygame.init()
+    # DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE,MAPHEIGHT*TILESIZE))
+    DISPLAYSURF = pygame.display.set_mode((675, 650))
+    backPic = pygame.image.load('Oak.jpg')
+    DISPLAYSURF.blit(pygame.transform.scale(backPic, (675, 650)), (0, 0))
+    fight = pygame.image.load('fight.png')
+    fightx1 = 250
+    fighty1 = 0
+    fightx2 = 0
+    fighty2 = 250
+    fightx3 = 500
+    fighty3 = 500
+    while True:
+        #get all the user events
+        for event in pygame.event.get():
+            #if the user wants to quit
+            if event.type == QUIT:
+                #and the game and close the window
+                pygame.quit()
+                sys.exit()
+
+        #loop through each row
+        for row in range(MAPHEIGHT):
+            #loop through each column in the row
+            for column in range(MAPWIDTH):
+                #draw the resource at that position in the tilemap, using the correct colour
+                pygame.draw.rect(DISPLAYSURF, colours[tilemap[row][column]], (column*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE))
+                DISPLAYSURF.blit(pygame.transform.scale(fight, (50, 50)), (fightx1, fighty1))
+                DISPLAYSURF.blit(pygame.transform.scale(fight, (50, 50)), (fightx2, fighty2))
+                DISPLAYSURF.blit(pygame.transform.scale(fight, (50, 50)), (fightx1, fighty3))
+                DISPLAYSURF.blit(pygame.transform.scale(fight, (50, 50)), (fightx3, fighty2))
+        #update the display
+        pygame.display.update()
+
 
 def text_object(text,font):
     textSurface = font.render(text,True,white)
@@ -31,9 +132,7 @@ y = (600 * 0.1)
 
 def button(action=None):
     mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()  # nieuwe functie die erop let dat er een mousebutton wordt ingedrukt
-    print(click)                        #print een stroom van (0, 0, 0) in de console, checkt of er een mousebutton wordt ingedrukt
-                                        #linkermuisknop = (1, 0, 0)     rechtermuisknop = (0, 0, 1)     middlemuisknop = (0, 1, 0)
+    click = pygame.mouse.get_pressed()
 
     if 400+100 > mouse[0] > 400 and 450+50 > mouse[1] > 450:
         pygame.draw.rect(display, dark_red,(400,450,100,50))
@@ -59,6 +158,8 @@ def button(action=None):
 
     if 100 + 100 > mouse[0] > 100 and 450 + 50 > mouse[1] > 450:
         pygame.draw.rect(display, dark_red,(100,450,100,50))
+        if click[0] == 1:
+            Gameboard()
     else:
         pygame.draw.rect(display, red, (100,450,100,50))
 
