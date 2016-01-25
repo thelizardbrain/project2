265c435f03cfbb9674fe475aa91b89843658d556
MexSperling <maxxsperling@hotmail.com>
2016-01-25 19:35:16 +0100

    Bijna volledig werkend samenhangend menu, 99% van de pep-fouten zijn eruitgehaald.

---
+++ Game_Complete.py
@@ -0,0 +1,211 @@
+import webbrowser, os
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+
+
+display = pygame.display.set_mode((800, 600))   # basis framework
+caption = pygame.display.set_caption('Survivor')
+fps = pygame.time.Clock()
+logoIMG = pygame.image.load('logo.png')
+tekst = pygame.font.Font('freesansbold.ttf', 20)
+
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 20, 0)
+dark_red = (150, 0, 0)
+
+
+
+def text_object(text, font):
+    textsurface = font.render(text, True, white)
+    return textsurface, textsurface.get_rect()
+
+
+def logo(x, y):
+    display.blit(logoIMG, (x, y))
+
+x = (800 * 0.211)
+y = (600 * 0.1)
+
+
+def button_main():
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if 400+100 > mouse[0] > 400 and 450+50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (400, 450, 100, 50))
+        if click[0] == 1:
+            webbrowser.open_new("file://" + os.path.realpath("Manual.pdf"))
+    else:
+        pygame.draw.rect(display, red, (400, 450, 100, 50))
+
+    if 550 + 100 > mouse[0] > 550 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (550, 450, 100, 50))
+        if click[0] == 1:
+            quit()
+    else:
+        pygame.draw.rect(display, red, (550, 450, 100, 50))
+
+    if 250 + 100 > mouse[0] > 250 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (250, 450, 100, 50))
+        if click[0] == 1:
+            options()
+    else:
+        pygame.draw.rect(display, red, (250, 450, 100, 50))
+
+    if 100 + 100 > mouse[0] > 100 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (100, 450, 100, 50))
+        if click[0] == 1:
+            player_select()
+    else:
+        pygame.draw.rect(display, red, (100, 450, 100, 50))
+
+    textsurf, textrect = text_object("QUIT", tekst)
+    textrect.center = ((550+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("INFO", tekst)
+    textrect.center = ((400+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("OPTIONS", tekst)
+    textrect.center = ((250+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("PLAY", tekst)
+    textrect.center = ((100+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+
+def button_options():
+
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if 400+100 > mouse[0] > 400 and 450+50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (400, 450, 100, 50))
+        if click[0] == 1:
+            print("window mode")
+            pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+    else:
+        pygame.draw.rect(display, red, (400, 450, 100, 50))
+
+    if 550 + 100 > mouse[0] > 550 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (550, 450, 100, 50))
+        if click[0] == 1:
+            main_menu()
+    else:
+        pygame.draw.rect(display, red, (550, 450, 100, 50))
+
+    if 150 + 100 > mouse[0] > 150 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (150, 450, 100, 50))
+        if click[0] == 1:
+            pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
+            print("fullscreen mode")
+    else:
+        pygame.draw.rect(display, red, (150, 450, 100, 50))
+
+    textsurf, textrect = text_object("Return", tekst)
+    textrect.center = ((550+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("Windowed", tekst)
+    textrect.center = ((400+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("Fullscreen", tekst)
+    textrect.center = ((150+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+
+def button_player():
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if 400+100 > mouse[0] > 400 and 450+50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (400, 450, 100, 50))
+        if click[0] == 1:
+            print("work in progress")
+    else:
+        pygame.draw.rect(display, red, (400, 450, 100, 50))
+
+    if 550 + 100 > mouse[0] > 550 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (550, 450, 100, 50))
+        if click[0] == 1:
+            main_menu()
+    else:
+        pygame.draw.rect(display, red, (550, 450, 100, 50))
+
+    if 250 + 100 > mouse[0] > 250 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (250, 450, 100, 50))
+        if click[0] == 1:
+            print("work in progress")
+
+    else:
+        pygame.draw.rect(display, red, (250, 450, 100, 50))
+
+    if 100 + 100 > mouse[0] > 100 and 450 + 50 > mouse[1] > 450:
+        pygame.draw.rect(display, dark_red, (100, 450, 100, 50))
+        if click[0] == 1:
+            print("work in progress")
+    else:
+        pygame.draw.rect(display, red, (100, 450, 100, 50))
+    textsurf, textrect = text_object("Return", tekst)
+    textrect.center = ((550+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("4 Players", tekst)
+    textrect.center = ((400+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("3 Players", tekst)
+    textrect.center = ((250+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+    textsurf, textrect = text_object("2 Players", tekst)
+    textrect.center = ((100+(100/2)), (450 + (50/2)))
+    display.blit(textsurf, textrect)
+
+
+def main_menu():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        display.fill(white)
+        logo(x, y)
+        button_main()
+        pygame.display.flip()
+        fps.tick()
+
+
+def player_select():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        display.fill(white)
+        logo(x, y)
+        button_player()
+        pygame.display.flip()
+        fps.tick()
+
+
+def options():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        display.fill(white)
+        logo(x, y)
+        button_options()
+        pygame.display.flip()
+        fps.tick()
+
+main_menu()
+pygame.quit()
+quit()
