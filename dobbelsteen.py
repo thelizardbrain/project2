2a582a365ac69223a2799c95c70905b267c574ef
MikeKouwenhoven <mkouwenhoven12@gmail.com>
2016-01-27 13:40:49 +0100

    .

--- dobbelsteen.py
+++ dobbelsteen.py
@@ -1,9 +1,14 @@
-import pygame as pg
+import pygame as pg, sys
+from pygame.locals import *
 import random
 
-pg.init()
-screen = pg.display.set_mode((800,600))
-screen_rect = screen.get_rect()
+class Button:
+    def __init__(self, screen_rect):
+        self.image = pg.Surface([100,50]).convert()
+        self.image.fill((255,0,0))
+        self.rect = self.image.get_rect(center=screen_rect.center)
+    def render(self, surf):
+        surf.blit(self.image, self.rect)
 
 def strip_from_sheet(sheet, start, size, columns, rows=1):
     frames = []
@@ -13,16 +18,15 @@ def strip_from_sheet(sheet, start, size, columns, rows=1):
             frames.append(sheet.subsurface(pg.Rect(location, size)))
     return frames
 
+pg.init()
+screen = pg.display.set_mode((800,600))
+screen_rect = screen.get_rect()
+
+
 dice_sheet = pg.image.load('dice.png')
 dice = strip_from_sheet(dice_sheet, (0,0), (36,36), 1, 6)
 
-class Button:
-    def __init__(self, screen_rect):
-        self.image = pg.Surface([100,50]).convert()
-        self.image.fill((255,0,0))
-        self.rect = self.image.get_rect(center=screen_rect.center)
-    def render(self, surf):
-        surf.blit(self.image, self.rect)
+
 
 image = pg.Surface([0,0]).convert()
 btn = Button(screen_rect)
@@ -34,7 +38,11 @@ while not done:
             done = True
         elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             if btn.rect.collidepoint(pg.mouse.get_pos()):
-                image = random.choice(dice)
-    screen.blit(image, (600,300))
+                rand = random.randint(0,5)
+                image = dice[rand]
+                print(rand + 1)
+
+    screen.blit(image, (500,100))
     btn.render(screen)
     pg.display.update()
+
