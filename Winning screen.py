f47a9d87ece2247bd1448c3bee1f95c2bccd7251
thelizardbrain <at_polat@hotmail.com>
2016-02-01 13:43:28 +0100

    ..

--- Winning screen.py
+++ Winning screen.py
@@ -5,6 +5,7 @@ display = pygame.display.set_mode((800,600))
 screen_rect = display.get_rect()
 white = (255,255,255)
 black = (0,0,0)
+red = (250,0,0)
 
 
 def winningscreen_object(text, font):
@@ -16,11 +17,12 @@ def winningscreen(text):
         largeText = pygame.font.Font('freesansbold.ttf',115)
         TextSurf, TextRect = winningscreen_object(text, largeText)
         TextRect.center = ((400),(300))
+        display.fill(red)
         display.blit(TextSurf, TextRect)
-
         pygame.display.update()
 
         time.sleep(5)
+
 
 
 
@@ -36,6 +38,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    message_display('Player Wins!')
+    winningscreen('Player Wins!')
     display.fill(white)
     pygame.display.update()
