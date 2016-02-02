625170264ab54e9ed792b90b5c4234067f259368
MexSperling <maxxsperling@hotmail.com>
2016-02-02 10:23:20 +0100

    Concept hoekfights

--- superfightcards/Hoekfight.py
+++ superfightcards/Hoekfight.py
@@ -6,14 +6,23 @@ import random
 pygame.init()
 
 
-if turn == "playerOne" and (bPy == 100 and 900 <= bPx <= 950) or (bPy == 150 and bPx == 950):
+if (bPy == 100, 900 <= bPx <= 950) or (bPy == 150, bPx == 950):
     hp_damage = hp2
-elif turn == "playerOne" and (bPy == 600 and 900 <= bPx <= 950) or (bPy == 550 and bPx == 950):
+    cnd_cost = cnpt1
+elif (bPy == 600, 900 <= bPx <= 950) or (bPy == 550, bPx == 950):
     hp_damage = hp3
-elif turn == "playerOne" and (bPy == 600 and 400 <= bPx <= 450) or (bPy == 550 and bPx == 450):
+    cnd_cost = cnpt1
+elif (bPy == 600, 400 <= bPx <= 450) or (bPy == 550, bPx == 450):
     hp_damage = hp4
+    cnd_cost = cnpt1
+elif (bPy == 100, 450 <= bPx <= 500) or (bPy == 150, bPx == 450):
+    cnpt1 = 15
+    if hp1 < 91:
+        hp1 += 10
+    else:
+        hp1 = 100
 
-def Hoekfight():
+def Hoekfight1():
     if rand == 1:
         #button1
             if click[0] == 1:
@@ -92,3 +101,99 @@ def Hoekfight():
             if click[0] == 1:
                 hp_damage -= 17
                 cpnt -= 5
+
+
+if turn == "playerTwo" and (bPy == 100, 900 <= bPx <= 950) or (bPy == 150, bPx == 950):
+    cnpt2 = 15
+    if hp2 < 91:
+        hp2 += 10
+    else:
+        hp2 = 100
+elif (bPy == 600, 900 <= bPx <= 950) or (bPy == 550, bPx == 950):
+    hp_damage = hp3
+    cnd_cost = cnpt2
+elif (bPy == 600, 450 <= bPx <= 500) or (bPy == 550, bPx == 450):
+    hp_damage = hp4
+    cnd_cost = cnpt2
+elif (bPy == 100, 450 <= bPx <= 500) or (bPy == 150, bPx == 450):
+    hp_damage = hp1
+    cnd_cost = cnpt2
+def Hoekfight2():
+    if rand == 1:
+        #button1
+            if click[0] == 1:
+                hp_damage -= 10
+                cnd_cost -= 2
+        #button2
+            if click[0] == 1:
+                hp_damage -= 20
+                cnd_cost -= 5
+        #button3
+            if click[0] == 1:
+                hp_damage -= 30
+                cnd_cost -= 8
+    elif rand == 2:
+        #button1
+            if click[0] == 1:
+                hp_damage -= 8
+                cnd_cost -= 3
+        #button2
+            if click[0] == 1:
+                hp_damage -= 13
+                cnd_cost -= 4
+        #button3
+            if click[0] == 1:
+                hp_damage -= 13
+                cnd_cost -= 17
+    elif rand == 3:
+        #button1
+            if click[0] == 1:
+                hp_damage -= 3
+                cnd_cost -= 1
+        #button2
+            if click[0] == 1:
+                hp_damage -= 9
+                cnd_cost -= 2
+        #button3
+            if click[0] == 1:
+                hp_damage -= 19
+                cnd_cost -= 3
+    elif rand == 4:
+        #button1
+            if click[0] == 1:
+                hp_damage -= 5
+                cnd_cost -= 2
+        #button2
+            if click[0] == 1:
+                hp_damage -= 11
+                cnd_cost -= 3
+        #button3
+            if click[0] == 1:
+                hp_damage -= 15
+                cnd_cost -= 5
+    elif rand == 5:
+        #button1
+            if click[0] == 1:
+                hp_damage -= 7
+                cnd_cost -= 2
+        #button2
+            if click[0] == 1:
+                hp_damage -= 12
+                cnd_cost -= 3
+        #button3
+            if click[0] == 1:
+                hp_damage -= 16
+                cnd_cost -= 4
+    elif rand == 6:
+        #button1
+            if click[0] == 1:
+                hp_damage -= 2
+                cnd_cost -= 1
+        #button2
+            if click[0] == 1:
+                hp_damage -= 4
+                cnd_cost -= 2
+        #button3
+            if click[0] == 1:
+                hp_damage -= 6
+                cnd_cost -= 3
