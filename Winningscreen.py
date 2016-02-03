c7793f8d78545e3ce08ec9726f94cc41cfb65957
thelizardbrain <at_polat@hotmail.com>
2016-02-03 21:59:32 +0100

    ..

--- Winningscreen.py
+++ Winningscreen.py
@@ -8,43 +8,6 @@ red = (250,0,0)
 dark_red = (150,0,0)
 tekst = pygame.font.Font('freesansbold.ttf',20)
 
-def button(action=None):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-
-
-    if 400+100 > mouse[0] > 400 and 450+50 > mouse[1] > 450:
-        pygame.draw.rect(display, dark_red,(400,450,100,50))
-        if click[0] == 1:
-                pygame.quit()
-    else:
-        pygame.draw.rect(display, red,(400,450,100,50))
-
-
-    if 600+100 > mouse[0] > 600 and 450+50 > mouse[1] > 450:
-        pygame.draw.rect(display, dark_red,(600,450,100,50))
-        if click[0] == 1:
-                pygame.quit()
-    else:
-        pygame.draw.rect(display, red,(600,450,100,50))
-
-
-
-    textSurf, textRect = text_object("QUIT",tekst)
-    textRect.center= ((400+(100/2)), (450 +(50/2)))
-    display.blit(textSurf,textRect)
-
-
-    textSurf, textRect = text_object("Return",tekst)
-    textRect.center= ((600+(100/2)), (450 +(50/2)))
-    display.blit(textSurf,textRect)
-
-
-def text_object(text,font):
-    textSurface = font.render(text,True,white)
-    return textSurface, textSurface.get_rect()
-
-
 
 
 
